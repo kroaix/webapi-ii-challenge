a61f9c4f430d978fd0f2d9367408992f398bff37
Katrina Roaix <kroaix@gmail.com>
2019-11-06 01:48:38 -0500

    MVP Complete

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const postsRouter = require('./posts/posts-router.js');
+
+const server = express();
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req, res) => {
+  res.send('Posts API');
+})
+
+module.exports = server;
